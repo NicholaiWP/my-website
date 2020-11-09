121c414d7fac9aabc46dd9cee064135a72f677b5
Nicholai <Nicholai_wp@hotmail.com>
2020-11-09 21:40:12 +0100

    edit comment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Services from './components/pages/Services';
 import Products from './components/pages/Products';
 import SignUp from './components/pages/SignUp';
 
-//app
+//app change
 function App() {
   return (
     <>
